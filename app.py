24ee64ad340d1afccd1a804882c7da754a3a4f97
gustjr123 <hs.cho9647@gmail.com>
2021-10-07 09:11:05 +0900

    start API

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+import subprocess
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/list')
+def list():
+    out = ""
+    try:
+        pid = subprocess.Popen(["ls", "-l"]).pid
+        out = subprocess.check_output(["ls", "-l"])
+    except EnvironmentError:
+        os.kill(pid)
+        print('unable to run subprocess')
+        return
+    return out
+
+if __name__ == '__main__':
+    app.run()
