2cd65191a3192b86fa77901e2146bd0225e958dd
ChoHyeonSeok <hs.cho9647@gmail.com>
2021-10-24 16:43:44 +0900

    test post

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api, reqparse
+# from datetime import datetime
+# from werkzeug.utils import secure_filename
 import dbModule
 import os
 
 app = Flask(__name__)
+api = Api(app)
+
+# UPLOAD_FOLDER_LOCATION = '/flask_api/app001/static/image'
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# app.config['UPLOAD_FOLDER_LOCATION'] = UPLOAD_FOLDER_LOCATION
+
+# # allowed_file 함수 > 허용된 형태의 파일 형식인지 확인
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def hello():
@@ -13,12 +26,25 @@ def hello():
 def list():
     db_class = dbModule.Database()
     db_name = os.environ.get('DB_NAME')
-    sql = "SELECT id, name FROM " + db_name + ".members"
+    sql = "SELECT id, name FROM " + db_name + ".test"
     row = db_class.executeAll(sql)
-
     print(row)
-
     return render_template('test.html', resultData=row)
+
+
+class Inputdata(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str)
+        parser.add_argument('path', type=str)
+        args = parser.parse_args()
+
+        name = args['name']
+        email = args['path']
+
+        return {'name': name, 'email': email}
+
+api.add_resource(Inputdata, '/camera')
 
 if __name__ == '__main__':
     app.run()
